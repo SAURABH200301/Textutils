58235b3da3e669849b47650ae444defdfa65f42e
sauryBhai <saurabhsharma200301@gmail.com>
2022-03-05 13:58:47 +0530

    started creating TextUtils

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import About from './components/About';
+import { useState } from 'react/cjs/react.development';
+import Alert from './components/Alert';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  // Link
+} from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000)
+  }
+  const RemoveBG=()=>{
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-secondary');
+    document.body.classList.remove('bg-danger');
+    document.body.classList.remove('bg-warning');
+  }
+  const handlemode = (cls) => {
+    RemoveBG();
+    if(cls=== null)
+    {
+      if(mode==='light')
+      {
+        setMode('dark');
+        document.body.style.backgroundColor = "#18416a";
+      }
+      else{
+        setMode('light');
+        document.body.style.backgroundColor = "white";
+      }
+    }
+    else{
+      document.body.classList.add(`bg-${cls}`);
+      setMode('dark');
+      document.body.style.backgroundColor = "#18416a";
+      showAlert(" mode enabled", cls)
+    }
+
+  }
+  return (
+    <>
+    <Router>
+      <div>
+        {/* NAVIGATION BAR */}
+        <Navbar title="TextUtils" aboutText="It's About Us" mode={mode} setModee={handlemode} />
+        {/* ALERT BAR */}
+        <Alert alert={alert} />
+        <Routes>
+
+          <Route exact path="/about" element={<About mode={mode}/>}/>
+          <Route exact path="/" element={
+            <div className="container">
+            <TextForm heading="Your Text will Analyze here" label="Enter Your Text Here " mode={mode} showAlert={showAlert} />
+          </div>
+          }/>
+            
+          
+        </Routes>
+
+      </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,76 @@
+// import React, { useState } from 'react'
+
+export default function About(props) {
+
+    // const [myStyle,setMystyle]=useState({     //create a state 
+    //     color: 'white',
+    //     backgroundColor: 'black'
+    // })
+    const myStyle = {
+        color:props.mode ==='light'?'black':'#234506',
+        // backgroundColor:props.mode==='dark'?'#18416a':'white'
+        backgroundColor:props.mode  
+
+    }
+
+    // const [btntext, setbtntext]=useState("Enable light Mode")
+    //   const toggleStyle=()=>{
+    //       if(myStyle.color === 'white'){
+    //           setMystyle({
+    //               color: 'black',
+    //               backgroundColor: 'white'
+    //           })
+    //           setbtntext("Enable Dark Mode");
+    //       }
+    //       else{
+    //         setMystyle({
+    //             color: 'white',
+    //             backgroundColor: 'black'
+    //         })
+    //         setbtntext("Enable light Mode");
+    //       }
+    //   }
+    return (
+        <div className="container" style={myStyle} >
+            <h1> About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className={`accordion-item`} style={myStyle}>
+                    <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <strong>Accordion Item #1</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item" style={myStyle}>
+                    <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <strong>Accordion Item #2</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+                <div className="accordion-item" style={myStyle}>
+                    <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        <strong>Accordion Item #3</strong>
+                        </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
